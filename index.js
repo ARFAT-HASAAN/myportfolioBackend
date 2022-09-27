c1bb9f6b4cd0e703c6bd98db487e56717f2c4069
ARFAT-HASAAN <arfuhtv757@gmail.com>
2022-09-27 10:48:26 +0600

    change

--- index.js
+++ index.js
@@ -15,35 +15,59 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 })
 
-async function run() {
-  try {
-    await client.connect()
-
-    const database = client.db('portfolio')
-    const MessageCollection = database.collection('MessageCollection')
-    const ProjectsCollection = database.collection('Projects')
-
-    app.post('/users', async (req, res) => {
-      const user = req.body
-      // console.log(user)
-      const result = await MessageCollection.insertOne(user)
-      res.send(result)
-    })
-
-    app.get('/projects', async (req, res) => {
-      const result = await ProjectsCollection.find().toArray()
-      res.send(result)
-    })
-  } finally {
-    // await client.close();
-  }
-}
-run().catch(console.dir)
-
 app.get('/', (req, res) => {
   res.send(' hello arfat!!')
 })
 
-app.listen(port, () => {
-  console.log('my port number is', port)
+app.get('/projects', async (req, res) => {
+  let database = client.db('portfolio')
+  const ProjectsCollection = database.collection('Projects')
+  const result = await ProjectsCollection.find().toArray()
+  res.send(result)
 })
+
+// app.get('/items/:my_item', async (req, res) => {
+//   let my_item = req.params.my_item
+//   let item = await client
+//     .db('my_db')
+//     .collection('my_collection')
+//     .findOne({ my_item: my_item })
+
+//   return res.json(item)
+// })
+
+client.connect((err) => {
+  if (err) {
+    console.error(err)
+    return false
+  }
+  // connection to mongo is successful, listen for requests
+  app.listen(port, () => {
+    console.log('my port number is', port)
+  })
+})
+
+// async function run() {
+//   try {
+//     await client.connect()
+
+//     const database = client.db('portfolio')
+//     const MessageCollection = database.collection('MessageCollection')
+//     const ProjectsCollection = database.collection('Projects')
+
+//     app.post('/users', async (req, res) => {
+//       const user = req.body
+//       // console.log(user)
+//       const result = await MessageCollection.insertOne(user)
+//       res.send(result)
+//     })
+
+//     app.get('/projects', async (req, res) => {
+//       const result = await ProjectsCollection.find().toArray()
+//       res.send(result)
+//     })
+//   } finally {
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir)
